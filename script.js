2335d62eeaf34dc4fb6536c4cdb7a35e3de00a99
vedsave54 <152853916+vedsave54@users.noreply.github.com>
2024-04-28 11:55:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function generatePassword() {
+    var length = 12; // Default password length
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+"; // Character set for password generation
+    var password = "";
+    for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    document.getElementById("password").value = password;
+}
